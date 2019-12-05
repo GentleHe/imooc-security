03830b3e346ddf051708ab6a18edef765f3a40d0
GentleHeInCompany <13789127308@163.com>
2019-12-05 10:24:37 +0800

    配置security验证

--- imooc-security-demo/src/main/java/com/imooc/security/SecurityApplication.java
+++ imooc-security-demo/src/main/java/com/imooc/security/SecurityApplication.java
@@ -1,13 +1,15 @@
 package com.imooc.security;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * @author 何国锋
  * @date 2019-12-01 19:08
  */
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.imooc")
+@EnableAutoConfiguration(exclude = {org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class})
 public class SecurityApplication {
 
     public static void main(String[] args) {
